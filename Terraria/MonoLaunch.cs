cbd7b8e9c8e94c8dca5dab58d392910835f399c8
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-06-10 20:16:05 -0500

    1.4.4.9

--- Terraria/MonoLaunch.cs
+++ Terraria/MonoLaunch.cs
@@ -1,12 +1,18 @@
 #if !WINDOWS
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Terraria;
 
+namespace Terraria;
+
 internal static class MonoLaunch
 {
-	private static void Main(string[] args) {
+	private static readonly Dictionary<string, IntPtr> assemblies = new Dictionary<string, IntPtr>();
+
+	private static void Main(string[] args)
+	{
 		AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs sargs) {
 			string resourceName = new AssemblyName(sargs.Name).Name + ".dll";
 			string text = Array.Find(typeof(Program).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
